367adbfc0b75a7b51ff4618201df0dae08528fda
Nick-ch <nd.chernikov@gmail.com>
2018-04-03 10:57:18 +0300

    add top-level arguments syntax

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,9 +3,10 @@
 import commander from 'commander';
 
 commander
+  .arguments('<firstConfig> <secondConfig>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.1.1', '-v, --version')
-  .option('-f, --format [type]', 'Output format')
+  .version('0.1.3', '-v, --version')
+  .option('-f, --format [type]', 'output format')
   .parse(process.argv);
 
 if (!commander.args.length) commander.help();
