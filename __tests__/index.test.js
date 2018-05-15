5c263cc71cec830ba952475126277eab4776e748
Nick-ch <nd.chernikov@gmail.com>
2018-05-15 13:54:55 +0300

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,5 +3,8 @@ import genDiff from '../src';
 
 test('genDiff test', () => {
   const expectedDiff = fs.readFileSync('__tests__/__fixtures__/expected-diff.txt', 'utf-8');
-  return expect(genDiff('before.json', 'after.json')).toEqual(expectedDiff);
+  const exampleBefore = '__tests__/__fixtures__/before.json';
+  const exampleAfter = '__tests__/__fixtures__/after.json';
+
+  return expect(genDiff(exampleBefore, exampleAfter)).toEqual(expectedDiff);
 });
