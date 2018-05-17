b54dec248d24104618c144d042fa938255572c5a
Nick-ch <nd.chernikov@gmail.com>
2018-05-17 22:48:32 +0300

    add test files for nested configs

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,25 +2,25 @@ import fs from 'fs';
 import genDiff from '../src';
 
 test('genDiff test - JSON', () => {
-  const expectedDiff = fs.readFileSync('__tests__/__fixtures__/expected-diff.txt', 'utf-8');
-  const exampleBefore = '__tests__/__fixtures__/before.json';
-  const exampleAfter = '__tests__/__fixtures__/after.json';
+  const expectedDiff = fs.readFileSync('__tests__/__fixtures__/expected-diff-flat.txt', 'utf-8');
+  const exampleBefore = '__tests__/__fixtures__/flat/before.json';
+  const exampleAfter = '__tests__/__fixtures__/flat/after.json';
 
   return expect(genDiff(exampleBefore, exampleAfter)).toEqual(expectedDiff);
 });
 
 test('genDiff test - YAML', () => {
-  const expectedDiff = fs.readFileSync('__tests__/__fixtures__/expected-diff.txt', 'utf-8');
-  const exampleBefore = '__tests__/__fixtures__/before.yml';
-  const exampleAfter = '__tests__/__fixtures__/after.yml';
+  const expectedDiff = fs.readFileSync('__tests__/__fixtures__/expected-diff-flat.txt', 'utf-8');
+  const exampleBefore = '__tests__/__fixtures__/flat/before.yml';
+  const exampleAfter = '__tests__/__fixtures__/flat/after.yml';
 
   return expect(genDiff(exampleBefore, exampleAfter)).toEqual(expectedDiff);
 });
 
 test('genDiff test - INI', () => {
-  const expectedDiff = fs.readFileSync('__tests__/__fixtures__/expected-diff.txt', 'utf-8');
-  const exampleBefore = '__tests__/__fixtures__/before.ini';
-  const exampleAfter = '__tests__/__fixtures__/after.ini';
+  const expectedDiff = fs.readFileSync('__tests__/__fixtures__/expected-diff-flat.txt', 'utf-8');
+  const exampleBefore = '__tests__/__fixtures__/flat/before.ini';
+  const exampleAfter = '__tests__/__fixtures__/flat/after.ini';
 
   return expect(genDiff(exampleBefore, exampleAfter)).toEqual(expectedDiff);
 });
