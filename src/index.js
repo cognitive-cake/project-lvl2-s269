9885cbf75737f0035efaafe2cdc088388e652b5b
Nick-ch <nd.chernikov@gmail.com>
2018-05-19 17:48:38 +0300

    rewrite render structure

--- src/index.js
+++ src/index.js
@@ -12,33 +12,30 @@ const stringify = (val, tabLvl) => {
   return val;
 };
 
+const render = (arr, tabLvl) => arr.map(({
+  key,
+  keyStatus,
+  value: [valBefore, valAfter],
+  children,
+}) => {
+  if (keyStatus === 'include' && _.isEqual(valBefore, valAfter)) {
+    return `${_.repeat(tab, tabLvl)}  ${key}: ${stringify(valBefore, tabLvl)}`;
+  }
+  if (keyStatus === 'include' && !(_.isEqual(valBefore, valAfter))) {
+    if (_.isPlainObject(valBefore) && _.isPlainObject(valAfter)) {
+      return `${_.repeat(tab, tabLvl)}  ${key}: {\n${_.flatten(render(children, tabLvl + 2)).join('\n')}\n${_.repeat(tab, tabLvl + 1)}}`;
+    }
+    return [`${_.repeat(tab, tabLvl)}+ ${key}: ${stringify(valAfter, tabLvl)}`, `${_.repeat(tab, tabLvl)}- ${key}: ${stringify(valBefore, tabLvl)}`];
+  }
+  if (keyStatus === 'added') {
+    return `${_.repeat(tab, tabLvl)}+ ${key}: ${stringify(valAfter, tabLvl)}`;
+  }
+  return `${_.repeat(tab, tabLvl)}- ${key}: ${stringify(valBefore, tabLvl)}`;
+});
+
 const renderDiff = (pathToFile1, pathToFile2) => {
   const ast = processData(pathToFile1, pathToFile2);
-
-  const render = (arr, tabLvl) => arr.map(({
-    key,
-    keyStatus,
-    value: [valBefore, valAfter],
-    children,
-  }) => {
-    if (keyStatus === 'include' && _.isEqual(valBefore, valAfter)) {
-      return `${_.repeat(tab, tabLvl)}  ${key}: ${stringify(valBefore, tabLvl)}`;
-    }
-    if (keyStatus === 'include' && !(_.isEqual(valBefore, valAfter))) {
-      if (_.isPlainObject(valBefore) && _.isPlainObject(valAfter)) {
-        return `${_.repeat(tab, tabLvl)}  ${key}: {\n${_.flatten(render(children, tabLvl + 2)).join('\n')}\n${_.repeat(tab, tabLvl + 1)}}`;
-      }
-      return [`${_.repeat(tab, tabLvl)}+ ${key}: ${stringify(valAfter, tabLvl)}`, `${_.repeat(tab, tabLvl)}- ${key}: ${stringify(valBefore, tabLvl)}`];
-    }
-    if (keyStatus === 'added') {
-      return `${_.repeat(tab, tabLvl)}+ ${key}: ${stringify(valAfter, tabLvl)}`;
-    }
-    return `${_.repeat(tab, tabLvl)}- ${key}: ${stringify(valBefore, tabLvl)}`;
-  });
-
-  const result = `{\n${_.flatten(render(ast, 1)).join('\n')}\n}`;
-
-  return result;
+  return `{\n${_.flatten(render(ast, 1)).join('\n')}\n}`;
 };
 
 export default renderDiff;
