b6ba60ebc31326002df07362204807898da5dc7f
Nick-ch <nd.chernikov@gmail.com>
2018-05-19 19:24:32 +0300

    remove MVC

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import genDiff from './view';
+import _ from 'lodash';
+import processData from './parser';
+import renderJsonDiff from './renderJson';
+
+const genDiff = (pathToFile1, pathToFile2) => {
+  const ast = processData(pathToFile1, pathToFile2);
+  return `{\n${_.flatten(renderJsonDiff(ast, 1)).join('\n')}\n}`;
+};
 
 export default genDiff;
+
