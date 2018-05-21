90eb325ce5126bb7ff751403a665ca696650b54d
Nick-ch <nd.chernikov@gmail.com>
2018-05-21 18:25:01 +0300

    change version number

--- src/rendering/index.js
+++ src/rendering/index.js
@@ -1,7 +1,7 @@
 import renderPlainDiff from './plainDiff';
 import renderPrettyDiff from './prettyDiff';
 
-// заготовка для шестого задания, это не окончательная реализация выбора рендера
+// not the final rendering implementation
 const renderDiff = (ast, options) => (options ? renderPlainDiff(ast) : renderPrettyDiff(ast, 1));
 
 export default renderDiff;
