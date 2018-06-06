76b911df4bcca68026f50ec4f62c76feb3efbf14
cognitive-cake <nd.chernikov@gmail.com>
2018-06-06 15:18:26 +0300

    add new options to CLI

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,6 +6,8 @@ import genDiff from '..';
 programm
   .description('Compares two configuration files and shows a difference.')
   .version('0.5.2', '-v, --version')
+  .option('-p --plain', 'Show diff in plain style')
+  .option('-P --pretty', 'Show diff in pretty style')
   .arguments('<firstConfig> <secondConfig>')
   .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)))
   .parse(process.argv);
